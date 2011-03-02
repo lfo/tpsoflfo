1e82bed4a7e12a1a8bb6d850de6d9102c301c957
laurent.foret <laurent.foret@ee290336-8dc2-734f-3e2e-ed15585c4b40>
2011-03-02 20:33:11 +0000

    start corrections for TP7

---
+++ 4jee-jpa-tp7-correction/src/test/java/com/ingesup/jee4/tp7/PersonDAOTest.java
@@ -0,0 +1,85 @@
+package com.ingesup.jee4.tp7;
+
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PersonDAOTest extends TestAbs {
+
+    public static final String DUPONT = "Dupont";
+    public static final String DURAND = "Durand";
+    public static final String JACQUES = "Jacques";
+    public static final String PAUL = "Paul";
+    public static final String PIERRE = "Pierre";
+    public static final String SMITH = "Smith";
+      
+
+    @Test
+    public void testDeleteAll() throws DAOException {
+        createPersons();
+        List<Person> persons = personDAO.getAllPersons();
+        assertFalse(persons.isEmpty());
+        deleteAll();
+        persons = personDAO.getAllPersons();
+        assertTrue(persons.isEmpty());
+    }
+
+    @Test
+    public void testCreate() throws DAOException {
+        deleteAll();
+        List<Person> persons = personDAO.getAllPersons();
+        assertTrue(persons.isEmpty());
+        createPersons();
+        persons = personDAO.getAllPersons();
+        assertEquals(3, persons.size());
+    }
+
+    @Test
+    public void testByPrefix() throws DAOException {
+        deleteAll();
+        createPersons();
+        List<Person> persons = personDAO.findAllWithPrefixLastName("Du");
+        assertEquals(2, persons.size());
+    }
+
+    @Test
+    public void testUpdate() throws DAOException {
+        deleteAll();
+        createPersons();
+        List<Person> smiths = personDAO.findAllWithPrefixLastName(SMITH);
+        assertEquals(1, smiths.size());
+        Person personToUpdate = smiths.iterator().next();
+        em.detach(personToUpdate);
+        personToUpdate.setLastName(DUPONT);
+        personDAO.updatePerson(personToUpdate);
+        smiths = personDAO.findAllWithPrefixLastName(SMITH);
+        assertTrue(smiths.isEmpty());
+        assertEquals(3, personDAO.getAllPersons().size());
+    }
+    
+    @Test 
+    public void testDelete() throws DAOException {
+        deleteAll();
+        createPersons();
+        List<Person> durands = personDAO.findAllWithPrefixLastName(DURAND);
+        assertEquals(1, durands.size());
+        Person personToDelete = durands.iterator().next();
+        personDAO.delete(personToDelete);
+        durands = personDAO.findAllWithPrefixLastName(DURAND);
+        assertTrue(durands.isEmpty());
+        assertEquals(2, personDAO.getAllPersons().size());
+    }
+
+    static void createPersons() throws DAOException {
+        personDAO.create(PIERRE, DUPONT);
+        personDAO.create(PAUL, DURAND);
+        personDAO.create(JACQUES, SMITH);
+    }
+
+    static void deleteAll() throws DAOException {
+        List<Person> persons = personDAO.getAllPersons();
+        for (Person person : persons) {
+            personDAO.delete(person);
+        }
+    }
+}
