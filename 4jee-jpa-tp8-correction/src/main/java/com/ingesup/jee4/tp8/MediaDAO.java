ea667bb81a60644b65223d80980e642b212cde57
laurent.foret <laurent.foret@ee290336-8dc2-734f-3e2e-ed15585c4b40>
2011-03-03 09:06:01 +0000

    add correction TP8

--- 4jee-jpa-tp8-correction/src/main/java/com/ingesup/jee4/tp8/MediaDAO.java
+++ 4jee-jpa-tp8-correction/src/main/java/com/ingesup/jee4/tp8/MediaDAO.java
@@ -15,5 +15,18 @@ public interface MediaDAO<M> {
     public M update(M media);
 
     public List<M> findByTitle(String titlePrefix);
-      
+    
+    /**
+     * Recherche des médias ayant au moins deux auteurs. 
+     * @return 
+     */
+    public List<M> findMediaWithAtLeast2Authors();
+
+    /**
+     * Recherche selon deux paramètres.
+     * @param titlePrefix peut être nul, dans ce cas le filtre se fait sur les auteurs 
+     * @param authors peut être nul ou vide, dans ce cas le filtre se fait sur le titre
+     * @return liste de Média si les deux paramètre sont vides retournent tous les médias.
+     */
+    public List<Media> findBy(String titlePrefix, List<Person> authors);
 }
