459550ce9a59575208df64e1aef08b193fb68cf9
laurent.foret <laurent.foret@ee290336-8dc2-734f-3e2e-ed15585c4b40>
2011-03-13 20:23:10 +0000

    tp9 correction start point

---
+++ trunk/4jee-jpa-tp9-correction/src/test/java/com/ingesup/jee4/tp9/business/BusinessTestAbs.java
@@ -0,0 +1,39 @@
+package com.ingesup.jee4.tp9.business;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author lforet
+ */
+public class BusinessTestAbs {
+
+    protected static EJBContainer ec;
+    protected static Context ctx;
+
+    // ======================================
+    // =          Lifecycle Methods         =
+    // ======================================
+    @BeforeClass
+    public static void initContainer() throws Exception {
+        Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put(EJBContainer.MODULES, new File("target/classes"));
+        properties.put("org.glassfish.ejb.embedded.glassfish.installation.root",
+                "./target/test-classes/glassfish");
+        ec = EJBContainer.createEJBContainer(properties);
+        ctx = ec.getContext();
+    }
+
+    @AfterClass
+    public static void closeContainer() throws Exception {
+        if (ec != null) {
+            ec.close();
+        }
+    }
+}
