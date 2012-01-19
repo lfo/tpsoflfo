0e2c9defeed910993762002a5144f2c4457141c0
laurent.foret@gmail.com <laurent.foret@gmail.com@ee290336-8dc2-734f-3e2e-ed15585c4b40>
2012-01-19 12:13:49 +0000

    introduce tp2 correction

--- trunk/jee-jpa-tp2/src/main/java/fr/devcoop/jee/tp2/PersonDAO.java
+++ trunk/jee-jpa-tp2/src/main/java/fr/devcoop/jee/tp2/PersonDAO.java
@@ -1,29 +1,10 @@
 package fr.devcoop.jee.tp2;
 
-import java.sql.SQLException;
-import java.util.List;
 
 /**
  *
  * @author lforet
  */
 public interface PersonDAO {
-
-    /**
-     * 
-     * 
-     * @return
-     * @throws DAOException
-     */
-    public List<Person> getAllPersons() throws DAOException;
-
-    public List<Person> findAllWithPrefixLastName(String prefixLastName)  throws DAOException;
-    
-    public void create(String firstName, String lastName) throws DAOException;
-    
-    public void updateLastName(Person person) throws DAOException;
-    
-    public void delete(Person person) throws DAOException;
-    
-    public void close() throws DAOException;
+// TODO ajouter les méthodes manquantes
 }
--- trunk/jee-jpa-tp2/src/main/java/fr/devcoop/jee/tp2/PersonDAOClient.java
+++ trunk/jee-jpa-tp2/src/main/java/fr/devcoop/jee/tp2/PersonDAOClient.java
@@ -1,7 +1,5 @@
 package fr.devcoop.jee.tp2;
 
-import java.util.List;
-
 
 /**
  *
@@ -9,42 +7,15 @@ import java.util.List;
  */
 public class PersonDAOClient {
 
-    public static PersonDAO getPersonDAO() throws DAOException {
-        return new PersonDAOImpl();
+    public static PersonDAO getPersonDAO() {
+        throw new UnsupportedOperationException("TODO");
     }
 
-    public static void main(String ... args) throws DAOException {
+    public static void main(String ... args)  {
         PersonDAO personDAO = getPersonDAO();
-        personDAO.create("Pierre", "Dupont");
-        personDAO.create("Paul", "Durand");
-        personDAO.create("Jacques", "Smith");
-        List<Person> persons = personDAO.getAllPersons();
-        for (Person person : persons) {
-            System.out.println(person.toString());
-        }
-        personDAO.close();
+       
+       // TODO test the DAO
         
-        persons = personDAO.findAllWithPrefixLastName("Du");
-        for (Person person : persons) {
-            System.out.println(person.toString());
-        }
-        
-        persons = personDAO.getAllPersons();
-        for (Person person : persons) {
-            if (person.getFirstName().equals("Jacques") && person.getLastName().equals("Smith")) {
-                person.setLastName("Dupont");
-                personDAO.updateLastName(person);
-            }
-            if (person.getFirstName().equals("Paul") && person.getLastName().equals("Durand")) {
-                personDAO.delete(person);
-            }
-        }
-
-        persons = personDAO.findAllWithPrefixLastName("Du");
-        for (Person person : persons) {
-            System.out.println(person.toString());
-        }
-        personDAO.close();
     }
 
 }
