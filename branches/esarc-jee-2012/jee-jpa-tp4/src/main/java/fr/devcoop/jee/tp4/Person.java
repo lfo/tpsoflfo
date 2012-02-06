d023fc47613052fb81b54448a3a7762fed6071e0
laurent.foret@gmail.com <laurent.foret@gmail.com@ee290336-8dc2-734f-3e2e-ed15585c4b40>
2012-02-06 15:07:18 +0000

    add TP4 in branch

---
+++ branches/esarc-jee-2012/jee-jpa-tp4/src/main/java/fr/devcoop/jee/tp4/Person.java
@@ -0,0 +1,47 @@
+package fr.devcoop.jee.tp4;
+
+/**
+ *
+ * @author lforet
+ */
+public class Person {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "firstName=" + firstName + "lastName=" + lastName + '}';
+    }
+        
+}
