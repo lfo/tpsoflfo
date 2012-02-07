c71cd84f067974b34536d6d75cbde49942b823e2
laurent.foret@gmail.com <laurent.foret@gmail.com@ee290336-8dc2-734f-3e2e-ed15585c4b40>
2012-02-07 08:57:37 +0000

    add TP5 in branch

---
+++ branches/esarc-jee-2012/jee-jpa-tp5/src/test/java/fr/devcoop/jee/tp5/TestAbs.java
@@ -0,0 +1,32 @@
+package fr.devcoop.jee.tp5;
+
+import fr.devcoop.jee.tp5.impl.PersonDAOJPAImpl;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author lfo
+ */
+public abstract class TestAbs {
+
+    protected static EntityManager entityManager;
+    protected static PersonDAOJPAImpl personDAO;
+//    protected static BookDAOJPAImpl bookDAO;
+
+    @BeforeClass
+    public static void init() throws Exception {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jee-tp5-pu");
+        entityManager = emf.createEntityManager();
+        personDAO = new PersonDAOJPAImpl(entityManager);
+//        bookDAO = new BookDAOJPAImpl(entityManager);
+    }
+
+    @AfterClass
+    public static void close() throws DAOException {
+        entityManager.close();
+    }
+}
