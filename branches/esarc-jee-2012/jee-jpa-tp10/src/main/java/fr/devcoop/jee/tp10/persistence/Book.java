77730af0ee2e36090745b6321d0220be14b0e268
laurent.foret@gmail.com <laurent.foret@gmail.com@ee290336-8dc2-734f-3e2e-ed15585c4b40>
2012-02-08 14:58:08 +0000

    add tp10 correction in branch

---
+++ branches/esarc-jee-2012/jee-jpa-tp10/src/main/java/fr/devcoop/jee/tp10/persistence/Book.java
@@ -0,0 +1,71 @@
+package fr.devcoop.jee.tp10.persistence;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+
+/**
+ *
+ * @author lforet
+ */
+@Entity
+@NamedQuery(name="allBooks", query="select b from Book b")
+public class Book implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    
+    @ManyToOne 
+    private Person owner;    
+    @ManyToMany
+    private List<Person> authors = new ArrayList<Person>();;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public Book() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Person> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<Person> authors) {
+        this.authors = authors;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+    
+}
---
+++ branches/esarc-jee-2012/jee-jpa-tp10/src/main/java/fr/devcoop/jee/tp10/persistence/Person.java
@@ -0,0 +1,86 @@
+package fr.devcoop.jee.tp10.persistence;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author lforet
+ */
+@Entity
+@NamedQuery(name="AllPersons", query="SELECT p FROM Person p")
+public class Person implements Serializable {
+
+    @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+    @OneToMany(mappedBy = "owner")
+    private List<Book> ownedBooks = new ArrayList<Book>();
+    @ManyToMany(mappedBy = "authors", fetch=FetchType.EAGER)
+    private List<Book> writtenBooks = new ArrayList<Book>();
+    
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person() {
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Book> getOwnedBooks() {
+        return ownedBooks;
+    }
+
+    public void setOwnedBooks(List<Book> ownedBooks) {
+        this.ownedBooks = ownedBooks;
+    }
+
+    public List<Book> getWrittenBooks() {
+        return writtenBooks;
+    }
+
+    public void setWrittenBooks(List<Book> writtenBooks) {
+        this.writtenBooks = writtenBooks;
+    }
+    
+    @Override
+    public String toString() {
+        return "Person{" + "firstName=" + firstName + "lastName=" + lastName + '}';
+    }
+    
+}
