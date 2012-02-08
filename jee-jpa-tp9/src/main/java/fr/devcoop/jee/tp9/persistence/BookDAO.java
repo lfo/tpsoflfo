6f0e2ded31902d5314b96bb4f4489fb5e90aefaa
laurent.foret <laurent.foret@gmail.com>
2012-02-08 09:20:53 +0000

    add tp9 in branch

---
+++ jee-jpa-tp9/src/main/java/fr/devcoop/jee/tp9/persistence/BookDAO.java
@@ -0,0 +1,23 @@
+package fr.devcoop.jee.tp9.persistence;
+
+import java.util.List;
+
+/**
+ *
+ * @author lforet
+ */
+public interface BookDAO {
+
+    public List<Book> getAllBooks() throws DAOException;
+    
+    public Book persistBook(Book book);
+
+    public Book updateBook(Book book);
+
+    public List<Book> findBookByTitle(String titlePrefix);
+
+    public Person getOwner(Book book);
+
+    public List<Person> getAuthors(Book book);
+    
+}
