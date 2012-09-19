7002766a1eebb6e6ada714b418dc9a1a4ef8722f
laurent.foret@gmail.com <laurent.foret@gmail.com@ee290336-8dc2-734f-3e2e-ed15585c4b40>
2012-09-19 14:27:37 +0000

    ajout énoncé rmi

---
+++ branches/esarc-jse-2012/distrib-bookstore1-rmi-enonce/src/main/java/com/bissy/distrib/bookstore/server/BookStoreServerMain.java
@@ -0,0 +1,8 @@
+package com.bissy.distrib.bookstore.server;
+
+public class BookStoreServerMain {
+
+    public static void main(String... args) {
+        throw new UnsupportedOperationException("TODO");
+    }
+}
