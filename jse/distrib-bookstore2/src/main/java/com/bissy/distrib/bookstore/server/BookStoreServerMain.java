52785aff90140110e166c91d48220cad9365bf97
laurent.foret <laurent.foret@gmail.com>
2013-01-13 11:32:25 +0000

    introduce tp1 in branch 2013

--- jse/distrib-bookstore2/src/main/java/com/bissy/distrib/bookstore/server/BookStoreServerMain.java
+++
@@ -1,8 +0,0 @@
-package com.bissy.distrib.bookstore.server;
-
-public class BookStoreServerMain {
-
-    public static void main(String... args) {
-        throw new UnsupportedOperationException("TODO");
-    }
-}
