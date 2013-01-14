289b7bda6e8941e45063434affa59466482a261f
laurent.foret <laurent.foret@ee290336-8dc2-734f-3e2e-ed15585c4b40>
2013-01-14 15:54:15 +0000

    add tp3-correction

---
+++ branches/esarc-jee-2013/jee-jpa-tp3-correction/src/test/java/fr/devcoop/jee/tp3/PersonDAOTest.java
@@ -0,0 +1,80 @@
+package fr.devcoop.jee.tp3;
+
+import fr.devcoop.jee.tp3.Person;
+import fr.devcoop.jee.tp3.PersonDAOImpl;
+import fr.devcoop.jee.tp3.DAOException;
+import java.util.List;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PersonDAOTest {
+    public static final String DUPONT = "Dupont";
+    public static final String DURAND = "Durand";
+    public static final String JACQUES = "Jacques";
+    public static final String PAUL = "Paul";
+    public static final String PIERRE = "Pierre";
+    public static final String SMITH = "Smith";
+
+    private static PersonDAOImpl personDAO = PersonDAOImpl.getInstance();
+
+    @BeforeClass
+    public static void initConnection() throws Exception {
+        personDAO.initConnection();
+    }
+
+    @AfterClass
+    public static void closeConnection() throws DAOException {
+        personDAO.close();
+    }
+    
+    @Test
+    public void testDeleteAll() throws DAOException {
+        createPersons();
+        List<Person> persons = personDAO.getAllPersons();
+        assertFalse(persons.isEmpty());
+        deleteAll();
+        persons = personDAO.getAllPersons();
+        assertTrue(persons.isEmpty());
+    }
+    
+    @Test
+    public void testCreate() throws DAOException {
+        deleteAll(); 
+        List<Person> persons = personDAO.getAllPersons();
+        assertTrue(persons.isEmpty());
+        createPersons();
+        persons = personDAO.getAllPersons();
+        assertEquals(3, persons.size());
+    }
+    
+    @Test 
+    public void testByPrefix() throws DAOException {
+        deleteAll();
+        createPersons();
+        List<Person> persons = personDAO.findAllWithPrefixLastName("Du");
+        assertEquals(2, persons.size());
+    }
+    
+    @Test 
+    public void testUpdate() throws DAOException {
+        deleteAll();
+        createPersons();
+        // TODO
+    }
+
+    private void createPersons() throws DAOException {
+        personDAO.create(PIERRE, DUPONT);
+        personDAO.create(PAUL, DURAND);
+        personDAO.create(JACQUES, SMITH);        
+    }
+    
+    private void deleteAll() throws DAOException {
+        List<Person> persons = personDAO.getAllPersons();
+        for (Person person : persons) {
+            personDAO.delete(person);
+        }
+    }
+    
+}
