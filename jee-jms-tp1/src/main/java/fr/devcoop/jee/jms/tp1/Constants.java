93a33fed5bd4178bb0398a9d343d24f01145f71f
laurent.foret <laurent.foret@ee290336-8dc2-734f-3e2e-ed15585c4b40>
2013-01-16 15:02:59 +0000

    add jms-tp1

---
+++ jee-jms-tp1/src/main/java/fr/devcoop/jee/jms/tp1/Constants.java
@@ -0,0 +1,11 @@
+package fr.devcoop.jee.jms.tp1;
+
+/**
+ *
+ * @author lforet
+ */
+public class Constants {
+
+    public static String BROKER_URL = "tcp://localhost:61616";
+    public static final String QUEUE_NAME = "jms-tp1";
+}
