725cbbb367443aa8e7f07d28efb16832dbaabd3f
laurent.foret <laurent.foret@gmail.com>
2013-07-17 09:53:50 +0000

    introduce RMIRegistryLauncher

--- distrib-bookstore2-correction/src/main/java/com/bissy/distrib/change/server/ChangeServerMain.java
+++ distrib-bookstore2-correction/src/main/java/com/bissy/distrib/change/server/ChangeServerMain.java
@@ -7,8 +7,8 @@ import java.rmi.registry.Registry;
 public class ChangeServerMain {
 
     public static void main(String... args) throws Exception {
-        Registry registry = LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
-        System.out.println("Registry launched ...");
+        Registry registry = LocateRegistry.getRegistry(Registry.REGISTRY_PORT);
+        System.out.println("Registry found ...");
         ChangeService changeService = new ChangeServiceRemoteImpl();
         registry.rebind(ChangeService.SERVICE_URL, changeService);
         System.out.println(ChangeService.SERVICE_URL + " bound ...");
