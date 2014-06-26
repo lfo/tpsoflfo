26989247051c9ce674c99aa012fd537a04ec98f9
laurent.foret@gmail.com <laurent.foret@gmail.com@ee290336-8dc2-734f-3e2e-ed15585c4b40>
2014-06-26 14:09:16 +0000

    correction prerequis.

---
+++ branches/esarc-jse-2014/distrib-prerequis-correction/src/main/java/com/bissy/distrib/sockets/threaded/EchoServerThread.java
@@ -0,0 +1,42 @@
+package com.bissy.distrib.sockets.threaded;
+
+import com.bissy.distrib.sockets.EchoProtocol;
+import com.bissy.distrib.sockets.ReverseEchoProtocol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class EchoServerThread extends Thread {
+
+    private Socket socket = null;
+
+    public EchoServerThread(Socket socket) {
+        super();
+        this.socket = socket;
+    }
+
+    public void run() {
+
+        try {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String inputLine, outputLine;
+            ReverseEchoProtocol echoProtocol = new ReverseEchoProtocol();
+
+            if ((inputLine = in.readLine()) != null) {
+//                outputLine = echoProtocol.processInput(inputLine);
+                out.println(inputLine);
+            }
+            out.close();
+            in.close();
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
