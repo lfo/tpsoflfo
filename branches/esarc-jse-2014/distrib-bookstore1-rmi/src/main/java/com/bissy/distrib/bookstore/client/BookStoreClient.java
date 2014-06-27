9b7aa960eb9b2f8b4a6d9658692b2e12bc0057b2
laurent.foret@gmail.com <laurent.foret@gmail.com@ee290336-8dc2-734f-3e2e-ed15585c4b40>
2014-06-27 12:38:27 +0000

    add distrib bookstore rmi

--- branches/esarc-jse-2014/distrib-bookstore1-rmi/src/main/java/com/bissy/distrib/bookstore/client/BookStoreClient.java
+++ branches/esarc-jse-2014/distrib-bookstore1-rmi/src/main/java/com/bissy/distrib/bookstore/client/BookStoreClient.java
@@ -1,30 +1,17 @@
 package com.bissy.distrib.bookstore.client;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Collection;
 
 import com.bissy.distrib.bookstore.Book;
-import com.bissy.distrib.bookstore.BookStoreService;
 
-public class BookStoreClient implements BookStoreService {
-	public static final String HOST_NAME = "localhost";
-	public static final int SERVER_PORT = 4444;
-	
-	private PrintWriter out;
-	private ObjectInputStream in;
-	private Socket socket;
-	
-	public Collection<Book> findAllBooks() throws Exception {
-		throw new UnsupportedOperationException("TODO");
-	}
+public class BookStoreClient {
 
-	public Collection<Book> findBooks(String title) throws Exception {
-		throw new UnsupportedOperationException("TODO");
-	}
+    public Collection<Book> findAllBooks() throws Exception {
+        throw new UnsupportedOperationException("TODO");
+    }
 
-	
+    public Collection<Book> findBooks(String title) throws Exception {
+        throw new UnsupportedOperationException("TODO");
+    }
+
 }
