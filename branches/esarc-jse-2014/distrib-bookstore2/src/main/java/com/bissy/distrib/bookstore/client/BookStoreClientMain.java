c685b8b9bac8c381c1cceaa30222f4ccd2e78140
laurent.foret@gmail.com <laurent.foret@gmail.com@ee290336-8dc2-734f-3e2e-ed15585c4b40>
2014-07-10 07:09:41 +0000

    Introduction du bookstore2

---
+++ branches/esarc-jse-2014/distrib-bookstore2/src/main/java/com/bissy/distrib/bookstore/client/BookStoreClientMain.java
@@ -0,0 +1,23 @@
+package com.bissy.distrib.bookstore.client;
+
+import com.bissy.distrib.bookstore.Book;
+import com.bissy.distrib.bookstore.BookFactory;
+import java.awt.EventQueue;
+import java.util.Collection;
+
+public abstract class BookStoreClientMain {
+
+    
+    public static void main(String ... args) {
+        Collection<Book> books = BookFactory.createBooks();
+        final BookViewer bookViewer = new BookViewer(null);
+        bookViewer.setBooks(books);
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                bookViewer.setVisible(true);
+            }
+        });
+       
+    }
+
+}
